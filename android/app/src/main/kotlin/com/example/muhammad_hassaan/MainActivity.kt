d22327490df9394969262e2f49f5f66722abebde
Muhammad Hassaan <muahmmadhassaanarain@gmail.com>
2021-05-30 00:45:36 -0700

    CV

---
+++ android/app/src/main/kotlin/com/example/muhammad_hassaan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.muhammad_hassaan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: (Text("Login Page")),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 200,
+              ),
+              Container(width: 200, child: TextField()),
+              Container(width: 200, child: TextField()),
+              SizedBox(
+                height: 100,
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("Login"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
